7a103304179cc275ddc4cdd5589606cd27800cf6
Zachary Ozer <zach@compass.com>
2016-06-11 10:17:38 -0400

    Adding JS matcher

---
+++ src/js/natchMatcher.js
@@ -0,0 +1,43 @@
+class NatchMatcher {
+  constructor(query, tokenizationSplitPattern, tokenizationCleanupPattern) {
+    this.tokenizationSplitPattern = tokenizationSplitPattern ?
+        tokenizationSplitPattern : /[^0-9a-z ]/g;
+    this.tokenizationCleanupPattern = tokenizationCleanupPattern ?
+        tokenizationCleanupPattern : /[\']/g;
+    this.tokenizedQuery = this.tokenize(query);
+  }
+
+  tokenize(text, debugOutput) {
+    text = text.toLowerCase()
+        .replace(this.tokenizationCleanupPattern, '')
+        .replace(this.tokenizationSplitPattern, ' ')
+        .split(' ');
+    const tokens = text.filter(token => token);
+    return tokens;
+  }
+  
+  matches(subject, key) {
+    // This assumes there will be fewer search tokens than subject tokens.
+    // If that's not generally the case, this should be reversed.
+    const subjectTokens = this.tokenize(key ? subject[key] : subject);
+    for (let searchToken of this.tokenizedQuery) {
+      let searchTokenInSubjectTokens = false;
+      for (let subjectToken of subjectTokens) {
+        if (subjectToken.startsWith(searchToken)) {
+          searchTokenInSubjectTokens = true;
+          break;
+        }
+      }
+      if (!searchTokenInSubjectTokens) {
+        return false;
+      }
+    }
+    return true;
+  }
+  
+  filter(subjects, key) {
+    return subjects.filter(subject => this.matches(subject, key));
+  }
+}
+
+export default NatchMatcher;
